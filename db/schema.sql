ce8dfd240d7e069f9cf5347f4dcc19fdeb31d97f
lunledev <183235731+lunledev@users.noreply.github.com>
2024-12-22 22:33:59 -0800

    minor changes

--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,13 @@
+\c postgres;
 
-drop table if exists employee_tracker_db;
+drop database if exists employee_tracker_db;
+
 create database employee_tracker_db;
+
 \c employee_tracker_db;
 
 drop table if exists department;
+
 create table department(
 id serial primary key,
 name varchar(30) unique not null
@@ -11,30 +15,29 @@ name varchar(30) unique not null
 
 );
 
+drop table if exists role cascade;
 
-drop table if exists role;
 create table role(
 id serial primary key,
-title varchar(30) unique not null
-salary decimal not null
-department_id integer not null
+title varchar(30) unique not null,
+salary decimal not null,
+department_id integer not null,
 foreign key(department_id)
 references department(id)
  on delete cascade
 );
 
 
-drop table if exists employee;
+drop table if exists employee cascade;
 create table employee(
 id serial primary key,
 first_name varchar(30) not null,
 last_name varchar(30) not null,
-role_id integer not null
+role_id integer not null,
 foreign key(role_id) 
 references role(id)
-on delete cascade
-,
-manager_id integer 
+on delete cascade,
+manager_id integer, 
 foreign key(manager_id)
 references employee(id)
 on delete set null
