283b0f4670a90134846f70921ec08dbe143aafdd
lunledev <183235731+lunledev@users.noreply.github.com>
2024-12-22 21:43:25 -0800

    added test sample for departement table

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+insert into department (name) values ('Engineering');
+insert into department (name) values ('Finance');
+insert into department (name) values ('Legal');
+insert into department (name) values ('Sales');
+
+
