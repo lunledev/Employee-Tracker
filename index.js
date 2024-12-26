bd4cde4981b9708b2808e211721218d873a8bf0d
lunledev <183235731+lunledev@users.noreply.github.com>
2024-12-25 16:54:11 -0800

    added initial menu

--- index.js
+++ index.js
@@ -1,49 +1,55 @@
 import { pool, connectToDb } from './connection.js';
 await connectToDb();
 
-import inquirer from 'inquirer'; 
+import inquirer from 'inquirer';
 
 
 
-const prompt =[
+const prompt = [
 
-{
+    {
 
-    type:'',
-    name:'',
-    message: 'What would you like to do?',
-    choices: [
+        type: 'list',
+        name: 'action',
+        message: 'What would you like to do?',
+        choices: [
+            'View All Employees',
+            'Add Employee',
+            'Update Employee Role',
+            'View All Roles',
+            'Add Role',
+            'View All Departments',
+            'Add Department',
+            'Quit',
+        ],
 
-
-    ],
-
-}
+    },
 
 
 
 ];
 
 
-
-/*
-What would you like to do?
-View All Employees
-Add Employee
-Update Employee Role
-View All Roles
-Add Role
-View All Departments
-Add Department
-Quit
-*/
-
-function init()
-{
+function init() {
     console.log('Welcome to Employee Tracker');
 
     const menuModule = inquirer.createPromptModule();
 
-    menuModule(prompt).then())=>{
+    menuModule(prompt).then((answers)=> {
+
+        if(answers.action ==='View All Employees')
+        {
+            pool.query(`SELECT * `);
+
+           //console.log();
+           //console.table();
+        }
+        else if(answers.action==='Quit')
+        {
+            pool.end();
+            process.exit();
+            
+        }
 
 
 
