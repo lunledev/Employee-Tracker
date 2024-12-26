7a35dad400fba24d8b9a90b5c2d56b17d0667558
lunledev <183235731+lunledev@users.noreply.github.com>
2024-12-25 16:35:43 -0800

    intial index.js

--- index.js
+++ index.js
@@ -4,10 +4,57 @@ await connectToDb();
 import inquirer from 'inquirer'; 
 
 
-//prompt menu
 
+const prompt =[
+
+{
+
+    type:'',
+    name:'',
+    message: 'What would you like to do?',
+    choices: [
+
+
+    ],
+
+}
+
+
+
+];
+
+
+
+/*
+What would you like to do?
+View All Employees
+Add Employee
+Update Employee Role
+View All Roles
+Add Role
+View All Departments
+Add Department
+Quit
+*/
+
+function init()
+{
+    console.log('Welcome to Employee Tracker');
+
+    const menuModule = inquirer.createPromptModule();
+
+    menuModule(prompt).then())=>{
+
+
+
+    });
+
+
+}
 
 
 //actions
 
 
+//prompt menu
+init();
