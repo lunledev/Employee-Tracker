09a8cd56e4a3ff620c0e59d10401298039bf7281
lunledev <183235731+lunledev@users.noreply.github.com>
2024-12-27 19:09:37 -0800

    minor changes

--- index.js
+++ index.js
@@ -29,45 +29,41 @@ const prompt = [
 
 ];
 
+function displayTable(result){
+    const header = Object.keys(result.rows[0]).join('\t');
 
-function init() {
-    console.log('Welcome to Employee Tracker');
+    console.table(`\n${header}\n--\t-----------`);
 
+    for (let i = 0; i < result.rows.length; i++) {
+        const rowdata = Object.values(result.rows[i]);
+        console.log(rowdata.join('\t'));
+    }
+    console.log('\n');
+
+}
+
+function menuActions() {
     const menuModule = inquirer.createPromptModule();
 
-    menuModule(prompt).then((answers) => {
 
+    menuModule(prompt).then((answers) => {
+        //actions
         if (answers.action === 'View All Departments') {
             pool.query('SELECT * FROM department', (err, result) => {
                 if (err) {
                     console.log(err);
                 }
                 else if (result) {
-                    const header = Object.keys(result.rows[0]).join('\t');
-
-                    console.table(`\n${header}\n--\t-----------`);
-
-                    for (let i = 0; i < result.rows.length; i++) {
-                        const rowdata = Object.values(result.rows[i]);
-                        console.log(rowdata.join('\t'));
-                    }
-
-                    
-                    
-                    
-
-
+                    displayTable(result);
+                    menuActions();
                 }
 
-
-
-
-
-
-
             });
+            
 
-            init();
+           
+
+            
 
 
         }
@@ -76,6 +72,11 @@ function init() {
 
             process.exit();
 
+
+        }
+        if(answers.action !== 'Quit')
+        {
+            menuActions();
 
         }
 
@@ -87,12 +88,20 @@ function init() {
 
     });
 
+}
+
+function init() {
+    console.log('Welcome to Employee Tracker');
+    menuActions();
+
+
+
 
 }
 
 
-//actions
 
 
-//prompt menu
+
+
 init();
